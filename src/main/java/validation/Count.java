26fcded843804c568820bd85b95f806206ec4fab
alenatokar87 <alenatokar87@gmail.com>
2017-05-25 11:59:55 +0300

    changed @Count

--- src/main/java/validation/Count.java
+++ src/main/java/validation/Count.java
@@ -11,18 +11,15 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-/**
- * Created by vov on 18.05.2017.
- */
 @NotNull
 @Min(value =1)
 @Max(value = 7)
+@ReportAsSingleViolation
 @Constraint(validatedBy ={})
 @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
 @Retention(RUNTIME)
-
 public @interface Count {
-    String message() default " {validation.Count.message}";
+    String message() default "{validation.Count.message}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
